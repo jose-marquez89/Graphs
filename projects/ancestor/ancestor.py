fe5f3d74bc41619c024834d78d0a382ce354e13e
jose-marquez89 <jlmarquez89@gmail.com>
2020-07-14 15:33:55 -0500

    adds first pass solution for ancestor function

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,22 @@
-
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    # build graph (adjaceny list)
+    ad_list = {}
+    for parent, child in ancestors:
+        if child not in ad_list:
+            ad_list[child] = {parent}
+        else:
+            ad_list[child] = ad_list[child].union({parent})
+
+    s = [starting_node]
+
+    while len(s) > 0:
+        earliest = s.pop()
+        for ancestor in ad_list[earliest]:
+            s.append(ancestor)
+
+    return earliest
+
+
+    
+
+
