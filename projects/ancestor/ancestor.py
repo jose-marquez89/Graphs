86edaf3c1859fb17a9acd3ed41d469f25a5e61ca
jose-marquez89 <jlmarquez89@gmail.com>
2020-07-14 19:54:57 -0500

    completes a working first pass solution for the ancestor problem

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -8,15 +8,30 @@ def earliest_ancestor(ancestors, starting_node):
             ad_list[child] = ad_list[child].union({parent})
 
     s = [starting_node]
+    preds = {}
+    generations = 0
+    preds[generations] = []
 
     while len(s) > 0:
         earliest = s.pop()
-        for ancestor in ad_list[earliest]:
-            s.append(ancestor)
+        if len(s) == 0:
+            new_gen = True
+        else:
+            new_gen = False
+        # ancestors with no known predecessors will not be in list
+        if earliest in ad_list:
+            for ancestor in ad_list[earliest]:
+                s.append(ancestor)
+        else:
+            preds[generations].append(earliest)
+        if new_gen:
+            generations += 1
+            preds[generations] = []
 
-    return earliest
-
-
-    
-
-
+    if earliest == starting_node:
+        return -1
+    else:
+        target = preds[generations-1]
+        if len(target) > 1:
+            return min(target)
+        return target.pop()
