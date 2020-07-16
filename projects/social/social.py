b8388a3dc0dc6d5fa749b8b918aec2b704048ce0
jose-marquez89 <jlmarquez89@gmail.com>
2020-07-16 10:21:20 -0500

    completes 'get_all_social_paths' function

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,8 +1,11 @@
 import random
+from queue import Queue
+
 
 class User:
     def __init__(self, name):
         self.name = name
+
 
 class SocialGraph:
     def __init__(self):
@@ -16,7 +19,8 @@ class SocialGraph:
         """
         if user_id == friend_id:
             print("WARNING: You cannot be friends with yourself")
-        elif friend_id in self.friendships[user_id] or user_id in self.friendships[friend_id]:
+        elif (friend_id in self.friendships[user_id] or
+              user_id in self.friendships[friend_id]):
             print("WARNING: Friendship already exists")
         else:
             self.friendships[user_id].add(friend_id)
@@ -26,7 +30,7 @@ class SocialGraph:
         """
         Create a new user with a sequential integer ID
         """
-        self.last_id += 1  # automatically increment the ID to assign the new user
+        self.last_id += 1  # automatically increment ID for new user
         self.users[self.last_id] = User(name)
         self.friendships[self.last_id] = set()
 
@@ -38,7 +42,8 @@ class SocialGraph:
         Creates that number of users and a randomly distributed friendships
         between those users.
 
-        The number of users must be greater than the average number of friendships.
+        The number of users must be greater than
+        the average number of friendships.
         """
         # Reset graph
         self.last_id = 0
@@ -64,8 +69,8 @@ class SocialGraph:
                     possible_friendships.append((x, y))
 
         random.shuffle(possible_friendships)
-
-        friend_i_verse = possible_friendships[:((avg_friendships*num_users)//2)]
+        total_friendships = avg_friendships*num_users
+        friend_i_verse = possible_friendships[:((total_friendships)//2)]
 
         # Add users
         for user in range(1, num_users+1):
@@ -77,7 +82,6 @@ class SocialGraph:
 
         return
 
-
     def get_all_social_paths(self, user_id):
         """
         Takes a user's user_id as an argument
@@ -87,9 +91,24 @@ class SocialGraph:
 
         The key is the friend's ID and the value is the path.
         """
-        visited = {}  # Note that this is a dictionary, not a set
-        # !!!! IMPLEMENT ME
-        return visited
+        paths = {}  # Note that this is a dictionary, not a set
+        node_q = Queue()
+        node_q.put(user_id)
+        while not node_q.empty():
+            current_user = node_q.get()
+            print(current_user)
+
+            if current_user not in paths:
+                paths[current_user] = [current_user]
+
+            for immediate in self.friendships[current_user]:
+                if immediate in paths:
+                    continue
+                paths[immediate] = list(paths[current_user])
+                paths[immediate].append(immediate)
+                node_q.put(immediate)
+
+        return paths
 
 
 if __name__ == '__main__':
@@ -98,4 +117,3 @@ if __name__ == '__main__':
     print(sg.friendships)
     connections = sg.get_all_social_paths(1)
     print(connections)
-
