acb630e689123793be337a4f665b81209e9a9ca6
jose-marquez89 <jlmarquez89@gmail.com>
2020-07-17 15:06:29 -0500

    adds passing tests for large map between 990-1005

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -3,13 +3,14 @@ from queue import Queue
 from ast import literal_eval
 from pdb import set_trace as bp
 import logging
+from pprint import pprint
 
 from player import Player
 from world import World
 
 FORMAT = "%(asctime)s - %(message)s"
 logging.basicConfig(level=logging.DEBUG, format=FORMAT)
-logging.disable(logging.DEBUG)
+# logging.disable(logging.DEBUG)
 # Load world
 world = World()
 
@@ -49,7 +50,10 @@ def room_bfs(initial: int, target: int, p_map: dict) -> list:
         cur = q.get()
         # print(cur)
         for direction in p_map[cur]:
-            q.put(p_map[cur][direction])
+            if p_map[cur][direction] != '':
+                q.put(p_map[cur][direction])
+            else:
+                continue
             if cur not in steps:
                 steps[p_map[cur][direction]] = [direction]
             else:
@@ -79,42 +83,63 @@ def path_finder(plyr):
     # while queue is not empty
     while not done:
         cur_room = plyr.current_room.id
+        logging.debug(f"Arrived at room {cur_room}")
         # fill out map
         if cur_room not in player_map:
             exits = plyr.current_room.get_exits()
             player_map[cur_room] = {e: '' for e in exits}
 
-        logging.debug(f"prv is {prv}")
         if prv is not None:
             player_map[cur_room][opposite_dirs[current_dir]] = prv
             player_map[prv][current_dir] = cur_room
-            logging.debug(f"executing player map fill for room {cur_room}")
 
         unexplored = [direction for direction in player_map[cur_room]
                       if player_map[cur_room][direction] == '']
 
-        logging.debug("Player map: " + str(player_map))
-        logging.debug(f"cur_room: {cur_room}, unexplored: {unexplored}")
-        logging.debug(f"change dir: {change_dir}")
-        logging.debug(f"nav_stack {nav_stack}")
 
-        if not prv:  # check if this will work sans condition 2
+        if prv is None:  # check if this will work sans condition 2
             if cur_room == starting_room:
                 if len(unexplored) == 0:
                     done = True
                     break
-            current_dir = random.choice(unexplored)
+            if len(unexplored) == 0:
+                # perform search and navigate back to room
+                logging.debug("PERFORMING A NON NORMAL SEARCH")
+                if len(nav_stack) > 0:
+                    origin_room = nav_stack.pop()
+                else:
+                    done = True
+                    break
+                # check if final room has been directionally exhausted
+                if len(nav_stack) == 0:
+                    if '' in player_map[origin_room].values():
+                        nav_stack.append(origin_room)
+                    else:
+                        done = True
+                        break
+                navigation = room_bfs(cur_room, origin_room, player_map)
+                logging.debug(f"attempting NON NORMALtraversal from {cur_room} to {origin_room}")
+                for step in navigation:
+                    plyr.travel(step)
+                    logging.debug(f"step {step}, room: {cur_room}")
+                    path.append(step)
+                    prv = None
+                continue
+            else:
+                current_dir = random.choice(unexplored)
 
         if len(unexplored) > 1:
             nav_stack.append(cur_room)
             # change directions if current is not available
             if current_dir in player_map[cur_room]:
                 plyr.travel(current_dir)
+                # logging.debug(f"step {current_dir}, room: {cur_room}")
                 path.append(current_dir)
                 prv = cur_room
             else:
                 current_dir = random.choice(unexplored)
                 plyr.travel(current_dir)
+                # logging.debug(f"step {current_dir}, room: {cur_room}")
                 path.append(current_dir)
                 prv = cur_room
         elif len(unexplored) == 0:
@@ -124,6 +149,7 @@ def path_finder(plyr):
             else:
                 done = True
                 break
+            logging.debug(f"Performing normal search from {cur_room} to {origin_room}")
             # check if final room has been directionally exhausted
             if len(nav_stack) == 0:
                 if '' in player_map[origin_room].values():
@@ -132,19 +158,24 @@ def path_finder(plyr):
                     done = True
                     break
             navigation = room_bfs(cur_room, origin_room, player_map)
+            logging.debug(f"Pending path: {navigation}")
+            logging.debug(f"attempting traversal from {cur_room} to {origin_room}")
             for step in navigation:
                 plyr.travel(step)
+                logging.debug(f"step {step}, room: {cur_room}")
                 path.append(step)
                 prv = None
         else:
             # change directions if player hits a corner
             if current_dir in player_map[cur_room]:
                 plyr.travel(current_dir)
+                # logging.debug(f"step {current_dir}, room: {cur_room}")
                 path.append(current_dir)
                 prv = cur_room
             else:
                 current_dir = unexplored.pop()
                 plyr.travel(current_dir)
+                # logging.debug(f"step {current_dir}, room: {cur_room}")
                 path.append(current_dir)
                 prv = cur_room
 
